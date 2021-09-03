37e606108d73bff39349b9ecab57079d2c0f5998
Nikita Kuzin <nickoskuzin@gmail.com>
2021-09-03 18:44:27 +0200

    add paradigm check

--- stem_order_validation.py
+++ stem_order_validation.py
@@ -14,12 +14,95 @@ with open('lexemes-V.txt','r',encoding = 'utf-8-sig') as input_file:
     text = input_file.read()
     lexemes = re.findall('-lexeme\n(?: [^\r\n]+\n)+', text, flags=re.DOTALL)
 
+def check_paradigm(root, paradigm):
+    """check if the lexemes in the file have the correct paradigm"""
+    root_type = ''
+    irregular = re.compile('to_(go|play|give|say)')
+    if re.match(irregular, paradigm):
+        paradigm_base = ''
+        paradigm_deviation = ''
+    else:
+        paradigm_label = re.search('_([VIQX]+)_*(.*)', paradigm)
+        paradigm_base = paradigm_label.group(1)
+        paradigm_deviation = paradigm_label.group(2)
+
+    #many redundant checks
+    if root.endswith('y'):
+        if root[1] == 'w':
+            root_type = '2w_3y'
+        else:
+            root_type = '3y'
+    elif root.endswith('n'):
+        if root[1] == 'w':
+            root_type = '2w_3n'
+        elif root[1] == 'y':
+            root_type = '2y_3n'
+        else:
+            root_type = '3n'
+    elif root.endswith('w'):
+        if root[1] == 'y':
+            root_type == '2y_3w'
+        else:
+            root_type = '3w'
+    elif root.endswith('r'):
+        if root[1] == 'y':
+            root_type = '2y_3r'
+        elif root[1] == 'w':
+            root_type = '2w_3r'
+        else:
+            root_type = '3r'
+    elif root.endswith('l'):
+        if root[1] == 'y':
+            root_type = '2y_3l'
+        elif root[1] == 'w':
+            root_type = '2w_3r'
+        else:
+            root_type = '3l'
+    elif root[1] == 'y':
+        if root.endswith('w'):
+            root_type = '2y_3w'
+        elif root.endswith('n'):
+            root_type = '2y_3n'
+        elif root.endswith('r'):
+            root_type = '2y_3r'
+        elif root.endswith('l'):
+            root_type = '2y_3l'
+        else:
+            root_type = '2y'
+    elif root[1] == 'w':
+        if root.endswith('n'):
+            root_type = '2w_3n'
+        elif root.endswith('r'):
+            root_type = '2w_3r'
+        elif root.endswith('y'):
+            root_type = '2w_3y'
+        elif root.endswith('l'):
+            root_type = '2w_3l'
+        else:
+            root_type = '2w'
+    elif root[0] == 'y':
+        if root.endswith('y'):
+            root_type = '1y_3y'
+        elif root.endswith('w'):
+            root_type = '1y_3w'
+        elif root.endswith('n'):
+            root_type = '1y_3n'
+        elif root.endswith('r'):
+            root_type = '1y_3r'
+        elif root.endswith('3l') == '1y_3l':
+            root_type = '1y_3l'
+        else:
+            root_type = '1y'
+
+        if root_type != paradigm_deviation:
+            print('Expected {0} but got {1} for root {2}.'.format(root_type, paradigm_deviation, root))
+
+
 stem_order_templates = read_stem_order('correct_stem_order.txt')
 
 with open('lexemes_order_test.txt', 'w', encoding = 'utf-8') as out_file:
     with open('missing_templates', 'w', encoding='utf-8') as err_file:
         for l in lexemes:
-            print(l)
             lex = re.search('lex: ([^\r\n]+)', l).group(1)
             paradigm = re.search('paradigm: ((?:V|to)_[^\r\n]*)', l).group(1)
             gramm = re.search('gramm: ([^\r\n]+)', l).group(1)
@@ -30,7 +113,8 @@ with open('lexemes_order_test.txt', 'w', encoding = 'utf-8') as out_file:
             if 'root:' in l:
                 root = re.search('root: *([^\r\n]+)', l).group(1)
             stamm = ''
-            if 'stamm:' in l:
+            stamm_match = re.compile(r'stamm:\s[^\r\n]+')
+            if 'stamm:' in l and re.match(stamm_match, l):
                 stamm = re.search('stamm: ([^\r\n]+)', l).group(1)
             trans_de = ''
             if 'trans_de:' in l:
@@ -38,6 +122,8 @@ with open('lexemes_order_test.txt', 'w', encoding = 'utf-8') as out_file:
             trans_en = ''
             if 'trans_en:' in l:
                 trans_en = re.search('trans_en: *([^\r\n]*)', l).group(1)
+
+            check_paradigm(root, paradigm)
 
             try:
                 new_stem_pattern = stem_order_templates[paradigm]
